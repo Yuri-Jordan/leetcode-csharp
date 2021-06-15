61c4534db0ddd704d7a215f68eb58eb1e3544999
Yuri Jordan <yurirdn@ufrn.edu.br>
2021-06-14 21:57:42 -0300

    feat(17): Letter Combinations Phone Number (implementa solução)

--- LeetCodeAlgoritmos/LetterCombinationsPhoneNumber.cs
+++ LeetCodeAlgoritmos/LetterCombinationsPhoneNumber.cs
@@ -5,11 +5,49 @@ namespace LeetCodeAlgoritmos
 {
     public class LetterCombinationsPhoneNumber
     {
+        private void recursao(
+            List<string> combinacoes,
+            List<string> teclado,
+            int indice,
+            string stringAtual,
+            string digits)
+        {
+            if (stringAtual.Length == digits.Length)
+            {
+                combinacoes.Add(stringAtual);
+                return;
+            }
+
+            string letrasDaTecla = teclado[digits[indice] - '0'];
+
+            for (int i = 0; i < letrasDaTecla.Length; i++)
+            {
+                recursao(combinacoes, teclado, indice + 1, stringAtual + letrasDaTecla[i], digits);
+            }
+        }
+
         public IList<string> LetterCombinations(string digits)
         {
-            
+            List<string> combinacoes = new List<string>();
+            if (digits == null || digits.Length == 0)
+            {
+                return combinacoes;
+            }
 
-            return new List<string>();
+            List<string> teclado = new List<string>();
+            teclado.Add("0");
+            teclado.Add("1");
+            teclado.Add("abc");
+            teclado.Add("def");
+            teclado.Add("ghi");
+            teclado.Add("jkl");
+            teclado.Add("mno");
+            teclado.Add("pqrs");
+            teclado.Add("tuv");
+            teclado.Add("wxyz");
+
+            recursao(combinacoes, teclado, 0, "", digits);
+            return combinacoes;
         }
     }
 }
